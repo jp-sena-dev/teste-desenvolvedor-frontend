ce02bb4cbae7a4ced2d67177489096462d595d22
jp-sena-dev <joaopedrosena1997@gmail.com>
2024-04-16 14:41:04 -0300

    :tada: first commit

---
+++ front-end/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.scss';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { LeafletForm } from './components/pages/leaflet-form ';
+import { LeafletResults } from './components/pages/leaflet-results';
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/bulario/name/:name?/company/:company?/id/:id?" element={<LeafletResults />}/>
+        <Route path="/" element={<LeafletForm />} />
+      </>
+    )
+  );
+
+  return (
+    <>
+      <h1>Consultas</h1>
+      <div className='divider'/>
+      <main>
+        <RouterProvider router={router} />
+      </main>
+    </>
+  )
+}
+
+export default App
